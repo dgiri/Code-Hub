798c6e091ba4675b1f7ac4dd8950905de60fb2d5
Debashis Giri <dgiri@crri.co.in>
2011-02-16 11:55:39 +0530

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewCodeHub::Application.config.secret_token = '1806825144c7b745c1d0ac78956993eb2aa4d5080b3f943e2e76cee425b803deda0b3c279aed1b7fd5cfce2048761b5ea703a97122cf92b1adc96ef0af2e091d'
