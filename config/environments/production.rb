3b2db518b1ae51d8c10e8ae45328cb013ba407aa
Debashis Giri <dgiri@crri.co.in>
2011-04-01 12:09:13 +0530

    host for production added.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ NewCodeHub::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { :host => 'new-code-hub.heroku.com' }
 end
