cfd73bd6fc8c23c281ac7c74ac51bcd3f22524bf
Debashis Giri <dgiri@crri.co.in>
2011-04-01 13:10:42 +0530

    testing mail sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,5 +48,5 @@ NewCodeHub::Application.configure do
   # config.active_support.deprecation = :notify
   # config.action_mailer.delivery_method = :sendmail
   ActionMailer::Base.delivery_method = :sendmail
-  # config.action_mailer.default_url_options = { :host => 'new-code-hub.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'new-code-hub.heroku.com' }
 end
