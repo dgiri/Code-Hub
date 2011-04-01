7817bf6ff8a63cfe732c94b5f28deac7dfabdee8
Debashis Giri <dgiri@crri.co.in>
2011-04-01 14:23:48 +0530

    trying gmail mail sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,19 @@ NewCodeHub::Application.configure do
 
   # Send deprecation notices to registered listeners
   # config.active_support.deprecation = :notify
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true 
-  config.action_mailer.raise_delivery_errors = true 
-  config.action_mailer.default_url_options = { :host => 'new-code-hub.heroku.com' }
+  # config.action_mailer.delivery_method = :sendmail
+  # config.action_mailer.perform_deliveries = true 
+  # config.action_mailer.raise_delivery_errors = true 
+  # config.action_mailer.default_url_options = { :host => 'new-code-hub.heroku.com' }
+  
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+     :address => "smtp.gmail.com",
+     :port => 587,
+     :domain => 'new-code-hub.heroku.com',
+     :user_name => 'giri.debu@gmail.com',
+     :password => 'begampur',
+     :authentication => 'plain',
+     :enable_starttls_auto => true } 
 end
